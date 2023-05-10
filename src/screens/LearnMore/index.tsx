ae9a65b48d06a670149127001af516e21214440e
sq3-paritosh <80150719+sq3-paritosh@users.noreply.github.com>
2023-05-10 11:08:04 +0100

    back button without functionality added on LearnMore

--- src/screens/LearnMore/index.tsx
+++ src/screens/LearnMore/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {LearnMoreLinks} from 'react-native/Libraries/NewAppScreen';
 import DefaultLayout from '../../layouts/defaultLayout';
+import {Button} from 'react-native';
 
 const LearnMore = (): JSX.Element => {
   return (
     <DefaultLayout>
       <LearnMoreLinks />
+      <Button title="Go back" />
     </DefaultLayout>
   );
 };
